8fc22fd765b09fc737d9cbb5ff3fc813e9262cc0
chaud1024 <chaud1024@gmail.com>
2024-04-26 14:28:01 +0900

    add emit: function deleteTranaction

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,12 +1,13 @@
 <template>
   <h3>History</h3>
+  <p v-if="transactionList.length === 0">No transactions added yet</p>
   <ul id="list" class="list">
     <li
       v-for="t in transactionList"
       :key="t.id"
       :class="t.amount < 0 ? 'minus' : 'plus'">
       {{ t.text }} <span>${{ t.amount }}</span>
-      <button class="delete-btn">x</button>
+      <button class="delete-btn" @click="deleteTransaction(t.id)">x</button>
     </li>
   </ul>
 </template>
@@ -20,6 +21,12 @@ const props = defineProps({
     required: true,
   },
 });
+
+const emit = defineEmits(["transactionDeleted"]);
+
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
 </script>
 
 <style lang="scss" scoped></style>
