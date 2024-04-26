19697cfda07a4dc8da1613b956f52bf2a311d8c4
chaud1024 <chaud1024@gmail.com>
2024-04-26 14:05:23 +0900

    delete +, - signs

--- src/components/IncomeExpense.vue
+++ src/components/IncomeExpense.vue
@@ -2,11 +2,11 @@
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p id="money-plus" class="money plus">+${{ props.income }}</p>
+      <p id="money-plus" class="money plus">${{ props.income }}</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-${{ props.expense }}</p>
+      <p id="money-minus" class="money minus">${{ props.expense }}</p>
     </div>
   </div>
 </template>
