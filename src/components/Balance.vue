7ddf5233e6da1f4a468c83bf711e391a2f3e2fde
chaud1024 <chaud1024@gmail.com>
2024-04-26 13:36:07 +0900

    add defineProps to get total data

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,8 +1,17 @@
 <template>
   <h4>Your Balance</h4>
-  <h5 id="balance">$0.00</h5>
+  <h5 id="balance">${{ total }}</h5>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
 
 <style lang="scss" scoped></style>
